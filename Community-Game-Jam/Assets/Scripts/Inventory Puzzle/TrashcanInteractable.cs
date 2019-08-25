1fe2609c9423f8560a1b860947a7caec0a4c5ff5
Lassi Niinikorpi <lassi.n94@gmail.com>
2019-08-25 20:27:34 +0300

    Keycards can be picked from trash cans Items can be viewed when inventory button is clicked

---
+++ Community-Game-Jam/Assets/Scripts/Inventory Puzzle/TrashcanInteractable.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashcanInteractable : MonoBehaviour, IInteractable
+{
+    public GameObject canvas;
+    public Item item;
+    public KeycardController keycardController;
+    public string GetName()
+    {
+        return name;
+    }
+
+    public void Interact()
+    {
+        if(item != null)
+        {
+            GameManager.instance.player.Inventory.Add(item);
+            KeycardController.instance.UpdateSlots(item);
+            item = null;
+        }
+        else
+        {
+            print("can is empty");
+        }
+    }
+
+    public void PlayerInRange()
+    {
+        canvas.gameObject.SetActive(true);
+    }
+
+    public void PlayerOutRange()
+    {
+        canvas.gameObject.SetActive(false);
+    }
+}
