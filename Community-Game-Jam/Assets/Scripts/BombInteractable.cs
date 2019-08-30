025b55aa9d88c2ae20259dd74bbe073fb595fb87
Lassi Niinikorpi <lassi.n94@gmail.com>
2019-08-30 21:01:19 +0300

    Some small adjustments

--- Community-Game-Jam/Assets/Scripts/BombInteractable.cs
+++ Community-Game-Jam/Assets/Scripts/BombInteractable.cs
@@ -13,8 +13,24 @@ public class BombInteractable : MonoBehaviour, IInteractable
 
     public void Interact()
     {
-        GameManager.instance.player.Inventory.Add(item);
-        KeycardController.instance.UpdateSlots(item);
+        bool found = false;
+        for (int i = 0; i < KeycardController.instance.inventorySlots.Count; i++)
+        {
+            if(KeycardController.instance.inventorySlots[i].GetComponent<InventorySlotButton>().itemInSlot == item)
+            {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            GameManager.instance.player.Inventory.Add(item);
+            KeycardController.instance.UpdateSlots(item); 
+        }
+        else
+        {
+            print("Bomb already in inventory");
+        }
     }
 
     public void PlayerInRange()
