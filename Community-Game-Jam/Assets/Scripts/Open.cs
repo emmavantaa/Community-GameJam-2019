84bc4ea3967036eca261d252fb177a5789e5eeec
Emma Vantaa <emma.vantaa@gmail.com>
2019-08-31 04:29:58 +0300

    Tavaraa
    Assetteja ja ääniä + Seinät ja colliderit

---
+++ Community-Game-Jam/Assets/Scripts/Open.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Open : MonoBehaviour
+{
+    Animator anim;
+    public Collider coll;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        coll = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetBool("isOpen", true);
+        }
+    }
+}
